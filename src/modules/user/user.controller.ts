73b44a1b229fc5c6dd062f941ca40d740d11e82a
Binh Nguyen <binh.nguyen@atech88.com>
2026-04-22 15:52:57 +0700

    fix: user controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,17 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 
-import CreateProductDTO from "./dto/create-user.dto";
 import UpdateProductDTO from "./dto/update-user.dto";
-import IProduct from "./user.interface";
 import { UserService } from "./user.service.impl";
+import CreateUserDTO from "./dto/create-user.dto";
+import IUser from "./user.interface";
 
 export default class UserController {
   public userService = new UserService();
 
   public createUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const model: CreateProductDTO = req.body;
-      const result: IProduct = await this.userService.createUser(model);
+      const model: CreateUserDTO = req.body;
+      const result: IUser = await this.userService.createUser(model);
 
       res.status(201).json(result);
     } catch (error) {
@@ -21,7 +21,7 @@ export default class UserController {
 
   public getAll = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const users: IProduct[] = await this.userService.getAll();
+      const users: IUser[] = await this.userService.getAll();
 
       res.status(200).json(users);
     } catch (error) {
@@ -33,7 +33,7 @@ export default class UserController {
     try {
       try {
         const userId: string = req.params.id;
-        const result: IProduct = await this.userService.getUserDetailById(userId);
+        const result: IUser = await this.userService.getUserDetailById(userId);
 
         res.status(200).json(result);
       } catch (error) {
@@ -49,7 +49,7 @@ export default class UserController {
       const model: UpdateProductDTO = req.body;
       const userId: string = req.params.id;
 
-      const updateUser: IProduct = await this.userService.updateUser(userId, model);
+      const updateUser: IUser = await this.userService.updateUser(userId, model);
 
       res.status(200).json(updateUser);
     } catch (error) {
@@ -60,7 +60,7 @@ export default class UserController {
   public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userId: string = req.params.id;
-      const result: IProduct = await this.userService.deleteUser(userId);
+      const result: IUser = await this.userService.deleteUser(userId);
 
       res.status(200).json(result);
     } catch (error) {
